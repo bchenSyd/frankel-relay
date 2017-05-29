8bee99478f3a5fa89ea795f7bb6c1ab4d8262cd9
Bo Chen <bo.chen@kindredgroup.com>
2017-05-29 11:06:13 +1000

    relay-runtime support

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,41 @@ var changed = require('gulp-changed');
 
 
 var destinationFolders = require('./targets')
-var RELAY_SRC = './*.js';
+var REACT_RELAY_SRC = './react-relay/**/*.js';
+var RELAY_RUNTIME_SRC = './relay-runtime/**/*.js';
 var RELAY_DEST = destinationFolders.map(dest => {
-    var dest = path.resolve(dest, 'node_modules/react-relay/')
-    console.log(`auto sync to ${dest}`)
-    return dest;}
-);
-
-// dev task
-gulp.task('default', ['relay-copy-source'], function () {
-    gulp.watch(RELAY_SRC, ['relay-copy-source']);
+    var dest = path.resolve(dest, 'node_modules/');
+    console.log(`auto sync to ${dest}/react-relay and  ${dest}/relay-runtime`);
+    return dest;
 });
 
-gulp.task('relay-copy-source', function () {
+// dev task
+gulp.task('default', ['relay runtime sync','relay sync'])
+gulp.task('relay runtime sync', ['relay-runtime-copy-source'], function () {
+    gulp.watch(RELAY_RUNTIME_SRC, ['relay-runtime-copy-source']);
+});
+
+gulp.task('relay sync', ['react-relay-copy-source'], function () {
+    gulp.watch(REACT_RELAY_SRC, ['react-relay-copy-source']);
+});
+
+gulp.task('react-relay-copy-source', function () {
     RELAY_DEST.forEach(dest => {
-        gulp.src(RELAY_SRC)
-            .pipe(changed(dest, { hasChanged: changed.compareSha1Digest }))
-            .pipe(gulp.dest(dest));
+        const relay_dest = path.join(dest, 'react-relay');
+        gulp.src(REACT_RELAY_SRC)
+            .pipe(changed(relay_dest, { hasChanged: changed.compareSha1Digest }))
+            .pipe(gulp.dest(relay_dest));
     })
 
 });
+gulp.task('relay-runtime-copy-source', function () {
+    RELAY_DEST.forEach(dest => {
+        const runtime_dest = path.join(dest, 'relay-runtime');
+        gulp.src(RELAY_RUNTIME_SRC)
+            .pipe(changed(runtime_dest, { hasChanged: changed.compareSha1Digest }))
+            .pipe(gulp.dest(runtime_dest));
+    })
+
+});
+
+
